GrapheneOS/platform_packages_modules_Bluetooth

Fix an OOB bug in btm_read_rssi_complete
Bug: 260569232
Test: manual, to add regression
Ignore-AOSP-First: security
Tag: #security
Change-Id: I3d56c64f205c3675ba3856c1e553878b945ec261

system/stack/acl/btm_acl.cc
@@ -1898,7 +1898,7 @@ void btm_read_rssi_timeout(UNUSED_ATTR void* data) {
  * Returns          void
  *
  ******************************************************************************/
-void btm_read_rssi_complete(uint8_t* p) {
+void btm_read_rssi_complete(uint8_t* p, uint16_t evt_len) {
   tBTM_CMPL_CB* p_cb = btm_cb.devcb.p_rssi_cmpl_cb;
   tBTM_RSSI_RESULT result;
 
@@ -1907,11 +1907,19 @@ void btm_read_rssi_complete(uint8_t* p) {
 
   /* If there was a registered callback, call it */
   if (p_cb) {
+    if (evt_len < 1) {
+      goto err_out;
+    }
+
     STREAM_TO_UINT8(result.hci_status, p);
     result.status = BTM_ERR_PROCESSING;
 
     if (result.hci_status == HCI_SUCCESS) {
       uint16_t handle;
+
+      if (evt_len < 4) {
+        goto err_out;
+      }
       STREAM_TO_UINT16(handle, p);
 
       STREAM_TO_UINT8(result.rssi, p);
@@ -1927,6 +1935,11 @@ void btm_read_rssi_complete(uint8_t* p) {
     }
     (*p_cb)(&result);
   }
+
+  return;
+
+ err_out:
+  LOG_ERROR("Bogus event packet, too short");
 }
 
 /*******************************************************************************

rd-stuffs/android_vendor_qcom_opensource_commonsys_system_bt

Merged-In: I3d56c64f205c3675ba3856c1e553878b945ec261
CRs-Fixed: 3425776
(cherry picked from commit 047a68b1efe612ea65758377b5698d1a69490413)

stack/btm/btm_acl.cc
@@ -2589,7 +2589,7 @@ void btm_read_rssi_timeout(UNUSED_ATTR void* data) {
  * Returns          void
  *
  ******************************************************************************/
-void btm_read_rssi_complete(uint8_t* p) {
+void btm_read_rssi_complete(uint8_t* p, uint16_t evt_len) {
   tBTM_CMPL_CB* p_cb = btm_cb.devcb.p_rssi_cmpl_cb;
   tBTM_RSSI_RESULT result;
   tACL_CONN* p_acl_cb = &btm_cb.acl_db[0];
@@ -2600,10 +2600,19 @@ void btm_read_rssi_complete(uint8_t* p) {
 
   /* If there was a registered callback, call it */
   if (p_cb) {
+    if (evt_len < 1) {
+      goto err_out;
+    }
+
     STREAM_TO_UINT8(result.hci_status, p);
 
     if (result.hci_status == HCI_SUCCESS) {
       uint16_t handle;
+
+      if (evt_len < 4) {
+        goto err_out;
+      }
+
       result.status = BTM_SUCCESS;
 
       STREAM_TO_UINT16(handle, p);
@@ -2625,6 +2634,11 @@ void btm_read_rssi_complete(uint8_t* p) {
 
     (*p_cb)(&result);
   }
+
+  return;
+
+err_out:
+  BTM_TRACE_ERROR("Bogus event packet, too short");
 }
 
 /*******************************************************************************
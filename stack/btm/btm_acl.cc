ed1664ead34fe09c6796a05b053042ce713d5e77
Hui Peng <phui@google.com>
2022-12-09 20:02:07 +0000

    Fix an OOB bug in btm_read_rssi_complete
    Bug: 260569232
    Test: manual, to add regression
    Tag: #security
    Ignore-AOSP-First: security
    Merged-In: I3d56c64f205c3675ba3856c1e553878b945ec261
    Change-Id: I3d56c64f205c3675ba3856c1e553878b945ec261
    
    CRs-Fixed: 3425776
    (cherry picked from commit 047a68b1efe612ea65758377b5698d1a69490413)

--- stack/btm/btm_acl.cc
+++ stack/btm/btm_acl.cc
@@ -2589,7 +2589,7 @@ void btm_read_rssi_timeout(UNUSED_ATTR void* data) {
  * Returns          void
  *
  ******************************************************************************/
-void btm_read_rssi_complete(uint8_t* p) {
+void btm_read_rssi_complete(uint8_t* p, uint16_t evt_len) {
   tBTM_CMPL_CB* p_cb = btm_cb.devcb.p_rssi_cmpl_cb;
   tBTM_RSSI_RESULT result;
   tACL_CONN* p_acl_cb = &btm_cb.acl_db[0];
@@ -2600,10 +2600,19 @@ void btm_read_rssi_complete(uint8_t* p) {
 
   /* If there was a registered callback, call it */
   if (p_cb) {
+    if (evt_len < 1) {
+      goto err_out;
+    }
+
     STREAM_TO_UINT8(result.hci_status, p);
 
     if (result.hci_status == HCI_SUCCESS) {
       uint16_t handle;
+
+      if (evt_len < 4) {
+        goto err_out;
+      }
+
       result.status = BTM_SUCCESS;
 
       STREAM_TO_UINT16(handle, p);
@@ -2625,6 +2634,11 @@ void btm_read_rssi_complete(uint8_t* p) {
 
     (*p_cb)(&result);
   }
+
+  return;
+
+err_out:
+  BTM_TRACE_ERROR("Bogus event packet, too short");
 }
 
 /*******************************************************************************
